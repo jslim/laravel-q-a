26df7d263b4f3d2e81e99b913cd5a204438e8763
Jose Delgado <josdelgaur@gmail.com>
2019-07-09 15:28:53 -0400

    question model

--- app/User.php
+++ app/User.php
@@ -36,4 +36,18 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function question(){
+
+        return $this->hasMany(Question::class);
+    }
+
+    public function setTtitleAttribute($value){
+
+        $this->attributes['title']=$value;
+        $this->attributes['title']=str_slug($value); //gives the title a slug format
+    }
+
+
 }
